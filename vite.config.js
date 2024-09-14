7fd4b0f0b59101930bb6a544962f37163eb64f66
Roberto <roberto.gonzalezrocha@yourgiving.co>
2024-09-14 18:07:42 +0100

    Generates the prod files (npm run build) in real production mode

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 /*
@@ -21,23 +21,30 @@ export default defineConfig({
 })
 */
 
-export default defineConfig({
-  plugins: [vue(), vueJsx()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default ({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return defineConfig({
+    plugins: [vue(), vueJsx()],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    },
+    build: {
+      emptyOutDir: false,
+      outDir: 'dist',
+      sourcemap: true,
+      lib: {
+        entry: {
+          dev: './src/main-dev.js',
+          prod: './src/main-prod.js'
+        },
+        formats: ['es', 'cjs']
+      }
+    },
+    define: {
+      'process.env': env
     }
-  },
-  build: {
-    emptyOutDir: false,
-    outDir: 'dist',
-    sourcemap: true,
-    lib: {
-      entry: {
-        dev: './src/main-dev.js',
-        prod: './src/main-prod.js'
-      },
-      formats: ['es', 'cjs']
-    }
-  }
-})
+  })
+}
